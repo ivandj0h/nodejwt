255faa8cdb6ac1af560902c5a06e63f73a5ccf1f
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-16 02:06:24 +0800

    modified auth by added hapi joi dependencies into the app

--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,35 @@ const router = require('express').Router();
 // Adding User Model
 const User = require('../model/User');
 
+// The Validation
+const Joi = require('@hapi/joi');
+const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+}
+
 //Creating The Actual Router Register
-router.post('/register', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err) {
-        res.status(400).send(err);
-    }
+router.post('/register', (req, res) => {
+
+    // Validate the data before creating a user
+    const validation = Joi.validate(req.body, schema);
+
+
+        res.send(validation);
+
+
+    // const user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    // try {
+    //     const savedUser = await user.save();
+    //     res.send(savedUser);
+    // }catch(err) {
+    //     res.status(400).send(err);
+    // }
 });
 
 // Export this Router
