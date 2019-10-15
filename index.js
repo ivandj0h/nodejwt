d920a78440fb021e30701596304d43ff33f819c2
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-15 23:19:34 +0800

    modified indexjs by added express json middleware, so it can read request body from the rest client

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Importing The Routes
+const authRoute = require('./routes/auth');
 
 // Adding Mongoose
 const mongoose = require('mongoose');
@@ -15,10 +17,11 @@ const mongoose = require('mongoose');
 // Connecting to Database
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('You\'re now Connected to the MongoDB Atlas...'));
 
-// Importing The Routes
-const authRoute = require('./routes/auth');
+// Middlewares
+app.use(express.json());
 
 // Route Middlewares
 app.use('/api/user', authRoute);
+
 // Running The NodeJS Server
 app.listen(3000, () => console.log('Server Running...'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,17 @@
 // Adding the Express Router
 const router = require('express').Router();
 
+// Adding User Model
+const User = require('../model/User');
+
 /* 
 Creating The Actual Router Register
 */
 router.post('/register', (req, res) => {
-    res.json({
-        id: 1,
-        content: "You're now Registered!"
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     });
 });
 
