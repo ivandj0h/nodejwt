8836300fb74a1e3f1eba70856e8d6b6704f325fa
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-15 23:06:23 +0800

    added User Model by added userSchema

---
+++ model/User.js
@@ -0,0 +1,30 @@
+// Adding Mongoose
+const mongoose = require('mongoose');
+
+// Creating MongoDb Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
